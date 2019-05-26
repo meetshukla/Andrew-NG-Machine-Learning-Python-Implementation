521a998a613dd2ee9f490e0f88f8570162abdd9d
meetshukla <meet22599@gmail.com>
2019-05-26 18:02:17 +0530

    added comments to multiple variable file

--- week1/Linear Regression with Multiple Variable.py
+++ week1/Linear Regression with Multiple Variable.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 
+#calculation of cost function
 def computeCostMulti(X, y, theta):
     temp = np.dot(X, theta) - y
     return np.sum(np.power(temp, 2)) / (2*m)
 
-
+#gradient descent calculation
 def gradientDescentMulti(X, y, theta, alpha, iterations):
     m = len(y)
     for _ in range(iterations):
@@ -15,7 +16,7 @@ def gradientDescentMulti(X, y, theta, alpha, iterations):
     return theta
 
 
-
+#read from dataset
 data = pd.read_csv('ex1data2.txt', sep = ',', header = None)
 X = data.iloc[:,0:2] # read first two columns into X
 y = data.iloc[:,2] # read the third column into y
@@ -28,13 +29,18 @@ X = np.hstack((ones, X))
 alpha = 0.01
 num_iters = 400
 theta = np.zeros((3,1))
+
+#convert rank1 arrays to rank2
 y = y[:,np.newaxis]
 
+#intital value of thetha
 J = computeCostMulti(X, y, theta)
 print(J)
 
+#for the new thetha 
 theta = gradientDescentMulti(X, y, theta, alpha, num_iters)
 print(theta)
 
+#calculate cost after updating the theta
 J = computeCostMulti(X, y, theta)
 print(J)
